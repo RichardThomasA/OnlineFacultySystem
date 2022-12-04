5510c566eb239dce219850ae14cb0fd8574f174a
Richard Thomas <richard.t.alappat@gmail.com>
2022-12-04 21:57:56 +0530

    minor fix for servlets

--- src/main/java/com/richard/javaservlet/AdminHomeServlet.java
+++ src/main/java/com/richard/javaservlet/AdminHomeServlet.java
@@ -3,11 +3,20 @@ package com.richard.javaservlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.richard.dao.User;
 
 public class AdminHomeServlet extends HttpServlet{
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		
 		System.out.println("reached admin home");
+		HttpSession session = request.getSession(false);
+		if(session!=null) {
+			System.out.println("active session");
+			User user = (User)session.getAttribute("user");
+		}
+			
 	}
 }
