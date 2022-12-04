263bd24bb66c850400ecb55fd3a44f3f3718a378
Richard Thomas <richard.t.alappat@gmail.com>
2022-12-04 18:16:36 +0530

    created user dao

---
+++ src/main/java/com/richard/dao/User.java
@@ -0,0 +1,42 @@
+package com.richard.dao;
+
+public class User {
+	
+	private int id;
+	private String userName;
+	private String password;
+	private String course;
+	private String userType;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+}
