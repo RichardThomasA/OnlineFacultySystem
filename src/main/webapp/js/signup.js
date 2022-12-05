fa4f0889c0d9977bfef78a0eae078707230ac0da
Richard Thomas <richard.t.alappat@gmail.com>
2022-12-05 23:49:52 +0530

    minor fix in js file

--- src/main/webapp/js/signup.js
+++ src/main/webapp/js/signup.js
@@ -4,8 +4,30 @@ window.onload = (loadEvent) =>{
         method : 'getAllCourses'
     })).then(response => response.json()).then(jsonData => {
             var jsonObject = JSON.stringify(jsonData);
-            var userArray =JSON.parse(jsonObject);
-            console.log(userArray);
+            //console.log(jsonObject);
+            var courseObject =JSON.parse(jsonObject);
+            console.log(courseObject);
+            const map = new Map(Object.entries(courseObject));
+            console.log(map);
             
+            var options = '';
+            var selectedKey = '';
+            const keyIterator = map.keys();
+            const valueIterator = map.values();
+            for( var i = 0;i<map.size; i++){
+                var value = valueIterator.next().value;
+                var key = keyIterator.next().value;
+                if(value=='All Courses'){
+                    selectedKey = key;
+                } 
+                console.log(key);
+                console.log(value);
+                console.log(document.getElementById("course-select").options.length);
+                document.getElementById("course-select").options.add(new Option(value,key));
+                //options+='<option value='+key+'>'+value+'</option>';
+            }
+            console.log(options);
+            //document.getElementById("course-select").innerHTML = options;
+            document.getElementById("course-select").value=selectedKey;
         });
 }
