6e2e6dd03a1dad3f38ac5f9fdc03ae9e9668f492
Richard Thomas <richard.t.alappat@gmail.com>
2022-12-06 19:56:32 +0530

    update table with feedback data

--- src/main/webapp/js/viewFeedback.js
+++ src/main/webapp/js/viewFeedback.js
@@ -13,6 +13,34 @@ function updateSelector(){
         }).then(response => response.json())
         .then(jsonData =>{
             console.log("json recieved");
+            var jsonObject = JSON.stringify(jsonData);
+            var table = '';
+            table = '<table class="table">'+
+            '<thead>'+
+            '<tr>'+
+                '<th scope="col">Question ID</th>'+
+                '<th scope="col">Faculty</th>'+
+                '<th scope="col">Student</th>'+
+                '<th scope="col">Question</th>'+
+                '<th scope="col">Answer</th>'+
+            '</tr>'+
+            '</thead>'+
+            '<tbody>';
+            var userArray =JSON.parse(jsonObject);
+            userArray.forEach((element) => {
+                console.log(element);
+                table +='<tr>';
+                table +='<td>'+element.fbId+'</td>';
+                table +='<td>'+element.facultyName+'</td>';
+                table +='<td>'+element.studentName+'</td>';
+                table +='<td>'+element.question+'</td>';
+                table +='<td>'+element.answer+'</td>';
+                table +='</tr>';
+            });
+            table +='</tbody>';
+            table +='</table>';
+
+            document.getElementById("feedback-table-container").innerHTML = table;
         });
     }
     else{
